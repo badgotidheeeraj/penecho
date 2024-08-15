db727a25bf7b8c9abc4d00a04664f8c9eb131ec9
badgotidheeraj <badgotidheeraj@gmail.com>
2024-08-15 14:36:01 +0530

    d

--- src/Component/Auth/Config.js
+++ src/Component/Auth/Config.js
@@ -1,7 +1,7 @@
 export const config = {
     appId:"f8448f27-dd2a-4a38-937b-3ceca9cfd89f",
     //    redirectUri: "http://localhost:3000",
-   redirectUri: "https://penecho.vercel.app/",
+   redirectUri: "https://penecho.vercel.app",
     scopes: [
         'user.read'
     ],
