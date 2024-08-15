588099edbc76364b576c2eb566c80fc262f65c7c
badgotidheeraj <badgotidheeraj@gmail.com>
2024-08-15 14:29:23 +0530

    cd

--- src/Component/Auth/Config.js
+++ src/Component/Auth/Config.js
@@ -1,7 +1,7 @@
 export const config = {
     appId:"f8448f27-dd2a-4a38-937b-3ceca9cfd89f",
     //    redirectUri: "http://localhost:3000",
-   redirectUri: `${process.env.REACT_APP_BASE_URL_OF_OWN}`,
+   redirectUri: "https://penecho.vercel.app/",
     scopes: [
         'user.read'
     ],
