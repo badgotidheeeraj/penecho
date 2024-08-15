b9203f9cea1e3d7702d8d6c92cbc6dedcc6727a4
badgotidheeraj <badgotidheeraj@gmail.com>
2024-08-15 13:57:09 +0530

    it

--- src/Component/Auth/Config.js
+++ src/Component/Auth/Config.js
@@ -1,6 +1,7 @@
 export const config = {
     appId:"f8448f27-dd2a-4a38-937b-3ceca9cfd89f",
-   redirectUri: `${process.env.REACT_APP_BASE_URL_OF_OWN}'//"http://localhost:3000",
+    //    redirectUri: "http://localhost:3000",
+   redirectUri: `${process.env.REACT_APP_BASE_URL_OF_OWN}`,
     scopes: [
         'user.read'
     ],
