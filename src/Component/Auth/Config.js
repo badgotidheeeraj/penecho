5323e17968133020ade6e1dbc775c280bc49b779
badgotidheeraj <badgotidheeraj@gmail.com>
2024-08-15 14:48:50 +0530

    xcxc

--- src/Component/Auth/Config.js
+++ src/Component/Auth/Config.js
@@ -1,10 +1,10 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 export const config = {
-    appId:"f8448f27-dd2a-4a38-937b-3ceca9cfd89f",
-       redirectUri: "http://localhost:3000",
-//    redirectUri: "https://penecho.vercel.app",
+    appId: "f8448f27-dd2a-4a38-937b-3ceca9cfd89f",
+    redirectUri: isLocalhost ? "http://localhost:3000" : "https://penecho.vercel.app",
     scopes: [
         'user.read'
     ],
     authority: "https://login.microsoftonline.com/common",
 };
-
